5b714333656174e416fd2c3b09b19833d7687038
zmyeo54 <zhiming.yeo@outlook.com>
2026-06-28 02:13:44 +0800

    Update useAuth.ts

--- client/src/_core/hooks/useAuth.ts
+++ client/src/_core/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { getLoginUrl } from "@/const";
-import { trpc } from "@/lib/trpc";
+import { trpc } from "../../lib/trpc";
 import { TRPCClientError } from "@trpc/client";
 import { useCallback, useEffect, useMemo } from "react";
 
